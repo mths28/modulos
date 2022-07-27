be4573500652c88ea9b3ef18dbe9b645f624ed64
Perkles <matheus12mendes@hotmail.com>
2022-07-26 21:01:57 -0300

    first commit

---
+++ main.mjs
@@ -0,0 +1,5 @@
+import { mostraIdade, mostraCidade, mostraHobby } from "./funcoes.mjs";
+
+console.log(mostraIdade("Matheus", 21))
+console.log(mostraCidade("Matheus", "São Paulo"))
+console.log(mostraHobby("Matheus", "dirigir"))
